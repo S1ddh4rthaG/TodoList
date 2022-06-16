e8c880acd792b0f55c7652a5e0db05b2857d0a7a
Siddhartha G <-->
2022-06-16 20:31:49 +0530

    Final Deploy.

---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+console.log("javascript reached.");
+let id = 1;
+
+$taskArea = $("#new-item");
+$todoListArea = $(".todo-list-items");
+$todoListDone = $(".todo-list-items-done");
+
+$todoListArea.on("click", "li input", function (event) {
+  $elmt = $(event.target).parent();
+
+  $elmt.children("span").addClass("completed");
+  $todoListDone.hide().prepend($elmt.clone()).fadeIn("fast");
+
+  $elmt.fadeOut("fast", function () {
+    $(this).remove();
+  });
+});
+
+$todoListDone.on("click", "li input", function (event) {
+  $elmt = $(event.target).parent();
+
+  $elmt.children("span").removeClass("completed");
+  $todoListArea.append($elmt.clone());
+
+  $elmt.fadeOut("fast", function () {
+    $(this).remove();
+  });
+});
+
+$("#add").on("click", function () {
+  let value = $taskArea.val();
+
+  $item = $(
+    `<li class="list-group-item text-wrap" id="${"item" + id++}">\
+        <input class="form-check-input me-1 p-2" type="checkbox">\
+        <span class="p-2 align-middle">${value}</span>\
+     </li>`
+  );
+
+  if (value !== "") {
+    $todoListArea.prepend($item);
+    $taskArea.val("");
+  }
+});
--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function () {
-  console.log("Sever started on port 3000.");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started.");
 });
